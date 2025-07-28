2c5a7922f04e70c3a3d3de328ac56d67d7b7cbfa
Naveen K <jaguarff66@gmail.com>
2025-07-28 23:00:19 +0530

    Create sql_generator.py

---
+++ sql_generator.py
@@ -0,0 +1,9 @@
+voicequery-ai-intelligent-speech-to-sql-database-query/
+├── app.py
+├── sql_generator.py        ✅ must be here
+├── speech_to_text.py
+├── query_parser.py
+├── database.db
+├── requirements.txt
+├── runtime.txt
+├── README.md
