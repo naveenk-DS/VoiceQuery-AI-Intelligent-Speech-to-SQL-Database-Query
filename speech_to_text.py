db56772a40aefa266a6366102939e1019d62c57a
Naveen K <jaguarff66@gmail.com>
2025-07-28 22:57:02 +0530

    Update speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,13 +1,7 @@
-# speech_to_text.py
-
 import speech_recognition as sr
-from googletrans import Translator
-
-
 
 def recognize_speech(language_code='en-US'):
     r = sr.Recognizer()
-    translator = Translator()
     
     with sr.Microphone() as source:
         print("🎤 Speak now...")
@@ -15,9 +9,7 @@ def recognize_speech(language_code='en-US'):
 
     try:
         text = r.recognize_google(audio, language=language_code)
-        translated = translator.translate(text, dest='en')
-        return translated.text
-
+        return text
     except sr.UnknownValueError:
         return "Sorry, could not understand the audio."
     except sr.RequestError:
